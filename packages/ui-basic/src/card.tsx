1b3243932e64a513804b54def7a291e3f27d9e3f
[kafunk] <kafunk@gmail.com>
2023-11-09 13:10:26 -0600

    rename web to basic; add and use ui package alternative

---
+++ packages/ui-basic/src/card.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+
+export function Card({
+  className,
+  title,
+  children,
+  href,
+}: {
+  className?: string;
+  title: string;
+  children: React.ReactNode;
+  href: string;
+}): JSX.Element {
+  return (
+    <a
+      className={className}
+      href={href}
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      <h2>
+        {title} <span>-&gt;</span>
+      </h2>
+      <p>
+        {children}
+      </p>
+    </a>
+  );
+}
